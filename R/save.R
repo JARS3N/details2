985220e6602cf0522467041f0211c70c14cf1a77
JARS3N <JARS3N@users.noreply.github.com>
2020-04-21 23:40:34 -0400

    Create save.R

---
+++ R/save.R
@@ -0,0 +1,12 @@
+save <- function(j,target) {
+  lapply(j, function(u) {
+    df <- details2::parse_dir(u)
+    name <-   paste0(basename(dirname(u)),
+                     "_",
+                     basename(u),
+                     "_machinevision_",
+                     Sys.Date(),
+                     ".csv")
+    newpath <- file.path(target, name)
+    write.csv(df, newpath, row.names = F)
+}
