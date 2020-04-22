c036e7351db3bda54975f3b753224142e156512c
JARS3N <JARS3N@users.noreply.github.com>
2020-04-22 01:46:57 -0400

    Update parse_dir.R

--- R/parse_dir.R
+++ R/parse_dir.R
@@ -1,11 +1,11 @@
 parse_dir<-function(DIR=choose.dir()){
-  require(parallel);
+  #require(parallel);
   dirs<-list.dirs(DIR,recursive=F)
   FLS <-sapply(dirs,list.files,pattern="details.xml$",full.names=T)
-  size.of.list <- length(FLS);
-  cl <- makeCluster( min(size.of.list, detectCores()) );
-  work<-parallel::parLapply(cl=cl,FLS,details2::attempt)
-  Out<-dplyr::bind_rows(work)
-  stopCluster(cl);
-  Out
+ # size.of.list <- length(FLS);
+  #cl <- makeCluster( min(size.of.list, detectCores()) );
+  #work<-parallel::parLapply(cl=cl,FLS,details2::attempt)
+  #Out<-dplyr::bind_rows(work)
+  #stopCluster(cl);
+  purrr::map_df(FLS,details2::attempt)
 }
