498b0b8469d61ee1542e7e06a981c1dd224092b8
JARS3N <JARS3N@users.noreply.github.com>
2020-04-22 00:09:41 -0400

    Update save.R

--- R/save.R
+++ R/save.R
@@ -1,9 +1,12 @@
-attempt <- function(u) {
-  tryCatch(
-    details2::parse(u),
-    error = function(e) {
-      NULL
-    },
-    finally = message(u)
-  )
+save <- function(j,target) {
+  lapply(j, function(u) {
+    df <- detals2::parse_dir(u)
+    name <-   paste0(basename(dirname(u)),
+                     "_",
+                     basename(u),
+                     "_machinevision_",
+                     Sys.Date(),
+                     ".csv")
+    newpath <- file.path(target, name)
+    write.csv(df, newpath, row.names = F)
 }
