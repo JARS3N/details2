de71d91944c785b0b5342e854a49c80dfb5d85a7
JARS3N <JARS3N@users.noreply.github.com>
2020-04-22 00:19:53 -0400

    Update parse_all.R

--- R/parse_all.R
+++ R/parse_all.R
@@ -1,6 +1,6 @@
 parse_all <- function() {
   target <- "/mnt/LSAG/Engineering/APPS ENG/MACHINE_VISION"
-  source <- "/mnt/Spotting/Logging"
+  source <- "/mnt/LSAG/Spotting/Logging"
   platforms <- list.dirs(path = source, recursive = F)
   lots <- unlist(lapply(platforms, list.dirs, recursive = F))
   details2::save(lots,target)
